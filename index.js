b0d873a33c0fe96faf40c99b2e82df0befd227fa
Abram Shook <ajshook77@gmail.com>
2022-05-02 10:08:16 -0500

    commit tree 172616d8055940179efe8f1e89c02fed76b946fe

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// titleCased(): returns an array with title case tutorial names.
+// Note that this function takes no arguments and should use the
+// global tutorials variable as data.
+// NOTE: This lab is challenging! You will need to iterate
+// through the tutorials array,
+// modifying the name of each tutorial.
+// To do this, you will also need to access and modify each individual word.
+
+// Some questions to consider:
+
+// How can we "iterate" through individual words in a string?
+// Can we execute an iteration inside an iteration? How?
+// How can we capitalize just the first letter in a word?
+// A couple of hints:
+
+// Break the task into smaller chunks: using the console or a REPL,
+// start by figuring out how to modify one individual element in the tutorials array.
+// Once you've got that working, then figure out how to update the array itself.
+// Use Google!!
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -8,9 +28,31 @@ const tutorials = [
   'Immutable State and Pure Functions',
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+  'what is JSONP?',
+]
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((element) => element.toUpperCase())
 }
+
+// const titleCased = () => {
+//   let newArr = [tutorials]
+//   return newArr.forEach((element) => element.toUpperCase())
+// }
+
+// const titleReturn = () => {
+//   return tutorials.forEach((element) => console.log(element.toUpperCase()))
+// }
+// titleReturn()
+
+// const tutorials = [
+//   'what does the this keyword mean?',
+//   'What is the Constructor OO pattern?',
+//   'implementing Blockchain Web API',
+//   'The Test Driven Development Workflow',
+//   ]
+// const titleReturn = (caps) => {
+//   for(i=0; i<tutorials.length; i++)
+//     console.log(`${caps}`.toUpperCase())
+// }
+// titleReturn(tutorials)
